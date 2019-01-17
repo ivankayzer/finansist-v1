75b8fc5d6c1a1496b8a2762a7dc43dd324bb9afa
Ivan Kayzer <ivan.kayzer@outlook.com>
2019-01-17 19:56:46 +0100

    Formatter WIP

--- resources/pages/formatter.vue
+++ resources/pages/formatter.vue
@@ -1,35 +1,59 @@
 <template>
   <div>
     <h1 class="title">Форматирование</h1>
-
-    <b-field>
-      <b-radio-button v-model="action"
-                      native-value="Nope"
-                      type="is-danger">
-        <b-icon icon="close"></b-icon>
-        <span>Nope</span>
-      </b-radio-button>
-
-      <b-radio-button v-model="action"
-                      native-value="Yep"
-                      type="is-success">
-        <b-icon icon="check"></b-icon>
-        <span>Yep</span>
-      </b-radio-button>
-
-      <b-radio-button v-model="action"
-                      native-value="Default">
-        Default
-      </b-radio-button>
-    </b-field>
+    <a class="button is-link is-rounded my-15" @click="addNew">Добавить новую</a>
+    <div class="block" v-for="(element, index) in actions">
+      <hr>
+      <a class="delete" @click="remove(index)"></a>
+      <b-radio v-model="element.action" type="is-dark"
+               native-value="crop">
+        Удалить текст
+      </b-radio>
+      <b-radio v-model="element.action" type="is-dark"
+               native-value="ignore">
+        Игнорировать
+      </b-radio>
+      <b-radio v-model="element.action" type="is-dark"
+               native-value="assign">
+        Приписать к категории
+      </b-radio>
+      <hr>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .my-15 {
+    margin: 15px 0;
+  }
+
+  .block {
+    margin-top: 10px;
+  }
+
+  .delete {
+    float: right;
+    margin-right: 30px;
+  }
+</style>
 
 <script>
   export default {
     data() {
       return {
-        action: ''
+        actions: []
+      }
+    },
+    methods: {
+      addNew() {
+        this.actions.push({
+          action: null,
+          match: null,
+          additional_data: null
+        })
+      },
+      remove(index) {
+        this.actions = this.actions.filter((element, i) => i !== index)
       }
     }
   }
