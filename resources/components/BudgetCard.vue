6cd7233c48f494560574825537cf9c09d16188f7
Ivan Kayzer <ivan.kayzer@outlook.com>
2019-02-09 15:09:41 +0100

    ProgressBar options

--- resources/components/BudgetCard.vue
+++ resources/components/BudgetCard.vue
@@ -15,7 +15,7 @@
         <div class="content">
           <div class="limit-container" v-for="(limit, index) in card.limits">
             <div class="limit-label">
-                {{ getCategoryById(limit.category_id).name }}
+              {{ getCategoryById(limit.category_id).name }}
             </div>
             <div class="limit" :data-id="index"></div>
           </div>
@@ -29,6 +29,12 @@
     </div>
   </div>
 </template>
+
+<style>
+  .limit-container {
+    position: relative;
+  }
+</style>
 
 <script>
   import daysjs from 'dayjs'
@@ -56,12 +62,37 @@
     computed: {
       categories() {
         return this.$store.getters.categories
-      },
+      }
     },
     mounted() {
       this.card.limits.forEach((limit, index) => {
-        new ProgressBar.Line(`[data-id="${index}"]`, {easing: 'easeInOut'}).animate(1);
-      });
+        new ProgressBar.Line(`[data-id="${index}"]`, {
+          strokeWidth: 4,
+          easing: 'easeInOut',
+          duration: 1400,
+          color: '#FFEA82',
+          trailColor: '#eee',
+          trailWidth: 1,
+          svgStyle: { width: '100%', height: '100%' },
+          text: {
+            style: {
+              color: '#999',
+              position: 'absolute',
+              right: '0',
+              top: '30px',
+              padding: 0,
+              margin: 0,
+              transform: null
+            },
+            autoStyleContainer: false
+          },
+          from: { color: '#FFEA82' },
+          to: { color: '#ED6A5A' },
+          step: (state, bar) => {
+            bar.setText(Math.round(bar.value() * 100) + ' %')
+          }
+        }).animate(1)
+      })
     }
   }
 </script>
